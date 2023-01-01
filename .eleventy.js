df4ac3a29bec3159907197075d00b127fbdb2526
Bryan Cook <cookbr@users.noreply.github.com>
2023-01-01 15:43:41 -0500

    Switched from Liquid to Nunjucks. Introduced eleventy-plugin-rss package to grab absoluteUrl filter. Added 'main' layout alias.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const eleventyRssPlugin = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addLayoutAlias("main", "layouts/main.njk");
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addNunjucksFilter(
+    "absoluteUrl",
+    eleventyRssPlugin.absoluteUrl
+  );
 };
