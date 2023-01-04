100282155657cbd7833ca4fe46b0a43f94d4d02a
Bryan Cook <cookbr@users.noreply.github.com>
2023-01-03 22:16:00 -0500

    Added responsive navigation bar at the top of the page.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const eleventyRssPlugin = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addLayoutAlias("main", "layouts/main.njk");
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addNunjucksFilter(
